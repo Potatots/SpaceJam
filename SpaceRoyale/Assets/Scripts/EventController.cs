7065b5c23f353574ff82a1a167e335afebcbafaf
Kosmik <malpa.w.klapkach@gmail.com>
2018-12-08 21:28:47 +0100

    Wreszcie sie udalo, Rewolucje i planety

---
+++ SpaceRoyale/Assets/Scripts/EventController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour {
+
+    public enum EventType
+    {
+        None,
+        Revolution,
+        Festival
+    };
+
+    public GameObject rocketPrefab;
+    public EventType eventType;
+
+    [SerializeField]
+    int delayFrames;
+
+    [SerializeField]
+    [Range(0,1)]
+    float revolutionProbability;
+
+
+    // Use this for initialization
+	void Start ()
+    {
+        eventType = EventType.None;
+        GetComponent<ParticleSystem>().enableEmission = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(eventType == EventType.None)
+        {
+            UpdateNone();
+        }
+        else
+        {
+
+        }
+    }
+
+    void UpdateNone()
+    {
+        if (Time.frameCount % delayFrames == 0)
+        {
+            float ran = Random.Range(0f, 1f);
+            if (ran < revolutionProbability)
+            {
+                eventType = EventType.Revolution;
+                GetComponent<ParticleSystem>().enableEmission = true;
+            }
+        }
+    }
+
+
+}
